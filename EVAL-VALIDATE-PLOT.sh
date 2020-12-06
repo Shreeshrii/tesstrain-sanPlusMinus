ca8d0420f9c27fe833eeea0af84a577d9222289f
Shree Devi Kumar <5095331+Shreeshrii@users.noreply.github.com>
2020-12-06 14:10:37 +0000

    update tsv to format table better on github

--- EVAL-VALIDATE-PLOT.sh
+++ EVAL-VALIDATE-PLOT.sh
@@ -16,9 +16,9 @@ make -f Makefile-Validate fasteval MODEL_NAME=${MODEL_NAME} VALIDATIONLIST=${VAL
 
 ## PLOT the LOGS
 
-grep 'best model' ${MODEL_LOG} |  sed  -e 's/^.*\///' |  sed  -e 's/\.checkpoint.*$//' | sed  -e 's/_/\t/g' > plot/tmp-${MODEL_NAME}-plot-best.tsv
-grep 'Eval Char' ${MODEL_LOG} | sed -e 's/^.*[0-9]At iteration //' | \sed -e 's/,.* Eval Char error rate=/\t\t/'  | sed -e 's/, Word.*$//' | sed -e 's/^/\t\t/'> plot/tmp-${MODEL_NAME}-plot-eval.tsv
-grep 'At iteration' ${MODEL_LOG} |  sed -e '/^Sub/d' |  sed -e '/^Update/d' | sed  -e 's/At iteration \([0-9]*\)\/\([0-9]*\)\/.*char train=/\t\t\1\t\2\t\t/' |  sed  -e 's/%, word.*$//'   > plot/tmp-${MODEL_NAME}-plot-iteration.tsv
+grep 'best model' ${MODEL_LOG} |  sed  -e 's/^.*\///' |  sed  -e 's/\.checkpoint.*$/\t\t\t/' | sed  -e 's/_/\t/g' > plot/tmp-${MODEL_NAME}-plot-best.tsv
+grep 'Eval Char' ${MODEL_LOG} | sed -e 's/^.*[0-9]At iteration //' | \sed -e 's/,.* Eval Char error rate=/\t\t/'  | sed -e 's/, Word.*$/\t\t/' | sed -e 's/^/\t\t/'> plot/tmp-${MODEL_NAME}-plot-eval.tsv
+grep 'At iteration' ${MODEL_LOG} |  sed -e '/^Sub/d' |  sed -e '/^Update/d' | sed  -e 's/At iteration \([0-9]*\)\/\([0-9]*\)\/.*char train=/\t\t\1\t\2\t\t/' |  sed  -e 's/%, word.*$/\t/'   > plot/tmp-${MODEL_NAME}-plot-iteration.tsv
 egrep "${VALIDATIONLIST}.log$|iteration" ${VALIDATIONLOG} > plot/tmp-${MODEL_NAME}-plot-${MODEL_NAME}-${VALIDATIONLIST}.LOG
 sed 'N;s/\nAt iteration 0, stage 0, /At iteration 0, stage 0, /;P;D'  plot/tmp-${MODEL_NAME}-plot-${MODEL_NAME}-${VALIDATIONLIST}.LOG | grep 'Eval Char' | sed -e "s/.${VALIDATIONLIST}.log.*Eval Char error rate=/\t\t\t/" | sed -e 's/, Word.*$//' | sed  -e 's/\(^.*\)_\([0-9].*\)_\([0-9].*\)_\([0-9].*\)\t/\1\t\2\t\3\t\4\t/g' >  plot/tmp-${MODEL_NAME}-plot-validation.tsv
 echo "Name	CheckpointCER	LearningIteration	TrainingIteration	EvalCER	IterationCER	ValidationCER" > plot/tmp-${MODEL_NAME}-plot-header.tsv
