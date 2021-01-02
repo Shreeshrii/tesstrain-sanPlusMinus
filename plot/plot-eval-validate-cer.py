978b0d1afd2802bec6994857778565605fb65e81
Shree Devi Kumar <5095331+Shreeshrii@users.noreply.github.com>
2021-01-02 12:20:12 +0000

    New plot scripts with best fit curve

--- plot/plot-eval-validate-cer.py
+++ plot/plot-eval-validate-cer.py
@@ -6,33 +6,47 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy.interpolate import UnivariateSpline
 
 arg_parser = argparse.ArgumentParser(
     '''Creates plot from Training and Evaluation Character Error Rates''')
-
 arg_parser.add_argument('-m', '--model', nargs='?',
                         metavar='MODEL_NAME', help='Model Name', required=True)
-
 arg_parser.add_argument('-v', '--validatelist', nargs='?', metavar='VALIDATELIST',
                         help='Validate List Suffix', required=True)
-
 args = arg_parser.parse_args()
 
-tsvfile = args.model + "-" + args.validatelist + "-plot_cer.tsv"
-plotfile = args.model + "-" + args.validatelist + "-plot_cer.png"
+maxticks=10
+maxy=10
 
-dataframe = pd.read_csv(tsvfile,sep='\t', encoding='utf-8')
-dataframe['TrainingIteration'] = dataframe['TrainingIteration'].fillna(-2)
-dataframe['TrainingIteration'] = dataframe['TrainingIteration'].astype(int)
-dataframe['TrainingIteration'] = dataframe['TrainingIteration'].astype(str)
-dataframe['TrainingIteration'] = dataframe['TrainingIteration'].replace('-2', np.nan)
+ytsvfile = "tmp-" + args.model + "-" + args.validatelist + "-iteration.tsv"
+ctsvfile = "tmp-" + args.model + "-" + args.validatelist + "-checkpoint.tsv"
+etsvfile = "tmp-" + args.model + "-" + args.validatelist + "-eval.tsv"
+vtsvfile = "tmp-" + args.model + "-" + args.validatelist + "-validate.tsv"
+plotfile = "../data/" + args.model + "/plot/" + args.model + "-" + args.validatelist + "-cer.png"
 
-t = dataframe['TrainingIteration']
-x = dataframe['LearningIteration']
-y = dataframe.IterationCER
-c = dataframe.CheckpointCER
-e = dataframe.EvalCER
-v = dataframe.ValidationCER
+ydf = pd.read_csv(ytsvfile,sep='\t', encoding='utf-8')
+cdf = pd.read_csv(ctsvfile,sep='\t', encoding='utf-8')
+edf = pd.read_csv(etsvfile,sep='\t', encoding='utf-8')
+vdf = pd.read_csv(vtsvfile,sep='\t', encoding='utf-8')
+
+ydf = ydf.sort_values('LearningIteration')
+cdf = cdf.sort_values('LearningIteration')
+edf = edf.sort_values('LearningIteration')
+vdf = vdf.sort_values('LearningIteration')
+
+y = ydf['IterationCER']
+x = ydf['LearningIteration']
+t = ydf['TrainingIteration']
+c = cdf['CheckpointCER']
+cx = cdf['LearningIteration']
+ct = cdf['TrainingIteration']
+e = edf['EvalCER']
+ex = edf['LearningIteration']
+et = edf['TrainingIteration'] # Not available in training log file
+v = vdf['ValidationCER']
+vx = vdf['LearningIteration']
+vt = vdf['TrainingIteration']
 
 trainlistfile = "../data/" + args.model + "/list.train"
 evalistfile = "../data/" + args.model + "/list.eval"
@@ -42,52 +56,69 @@ trainlistlinecount = len(open(trainlistfile).readlines(  ))
 evallistlinecount = len(open(evalistfile).readlines(  ))
 validatelistlinecount = len(open(validatelistfile).readlines(  ))
 
-maxxlimit=250000
-minxlimit=-100
-maxticks=10
-ymax = y[np.argmax(y)] # Use to limit y axis to Max IterationCER
-cmax = c[np.argmax(c)] # Use to limit y axis to Max CheckpointCER
-maxCERtoDisplay=10 # Use ymax/cmax, for more detail use 20 or lower
-minCERtoDisplay=-1 # Use -5 with ymax/cmax, -1 with 20 or lower
-
-def annot_min(boxcolor, xpos, ypos, x,y):
-    xmin = x[np.argmin(y)]
-    ymin = y.min()
-    boxtext= "{:.3f}% at {:.0f}" .format(ymin,xmin)
+def annot_min(boxcolor, xpos, ypos, x, y, z):
+    if z.isnull().values.any():
+          xmin = x[np.argmin(y)]
+          ymin = y.min()
+          boxtext= "{:.3f}% CER\n  at {:.0f} iterations" .format(ymin,xmin)
+    else:
+          tmin = z[np.argmin(y)]
+          xmin = x[np.argmin(y)]
+          ymin = y.min()
+          boxtext= "{:.3f}%  CER\n  at {:.0f} / {:.0f} iterations" .format(ymin,xmin,tmin)
     ax1.annotate(boxtext, xy=(xmin, ymin), xytext=(xpos,ypos), textcoords='offset points',
-            arrowprops=dict(shrinkA=1, shrinkB=1, fc='black', ec='white', connectionstyle="arc3"),
-            bbox=dict(boxstyle='round,pad=0.2', fc=boxcolor, alpha=0.3))
+        arrowprops=dict(shrinkA=1, shrinkB=1, fc=boxcolor,alpha=0.3, ec='white', connectionstyle="arc3"),
+        bbox=dict(boxstyle='round,pad=0.2', fc=boxcolor, alpha=0.3))
 
 PlotTitle="Tesseract LSTM Training - Model Name = " + args.model + ", Validation List = list." + args.validatelist
 fig = plt.figure(figsize=(11,8.5)) #size is in inches
 ax1 = fig.add_subplot()
 
-ax1.set_ylim([minCERtoDisplay,maxCERtoDisplay])
 ax1.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax1.yaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter("%.1f"))
 ax1.set_ylabel('Character Error Rate %')
 
-ax1.set_xlim([minxlimit,maxxlimit])
 ax1.set_xlabel('Learning Iterations')
 ax1.set_xticks(x)
 ax1.tick_params(axis='x', rotation=45, labelsize='small')
 ax1.locator_params(axis='x', nbins=maxticks)  # limit ticks on x-axis
 ax1.grid(True)
 
-ax1.plot(x, y, 'teal', alpha=0.7, label='CER every 100 Training Iterations', linewidth=0.5)
-if not c.dropna().empty: # not NaN or empty
-	ax1.scatter(x, c, c='teal', s=10, label='Checkpoints CER  from lstmtraining (list.train - ' + str(trainlistlinecount) +' lines)', alpha=0.7)
-	annot_min('teal',-0,-30,x,c)
-
-if not e.dropna().empty: # not NaN or empty
-#	ax1.plot(x, e, 'magenta', alpha=0.7)
-	ax1.scatter(x, e, c='magenta', s=10, label='Evaluation CER from lstmtraining (list.eval - ' + str(evallistlinecount) +' lines)', alpha=0.7)
-	annot_min('magenta',-0,30,x,e)
+ax1.scatter(x, y, c='teal', alpha=0.5, s=0.5, label='CER every 100 Training Iterations')
+ax1.plot(x, y, 'teal', alpha=0.5, linewidth=0.5, label='Training CER')
 
 if not v.dropna().empty: # not NaN or empty
-#	ax1.plot(x, v, 'maroon', alpha=0.7)
-	ax1.scatter(x, v, c='maroon', s=10, label='Validation CER from lstmeval (list.'  + args.validatelist + ' - ' + str(validatelistlinecount) +' lines)', alpha=0.7)
-	annot_min('maroon',-0,60,x,v)
+#	ax1.plot(vx, v, 'maroon', linestyle='dotted')
+	ax1.scatter(vx, v, c='maroon', s=12,
+    label='Validation CER from lstmeval (list.'  + args.validatelist +
+    ' - ' + str(validatelistlinecount) +' lines)', alpha=0.5)
+	annot_min('maroon',-30,60,vx,v,vt)
+
+if not e.dropna().empty: # not NaN or empty
+#	ax1.plot(ex, e, 'magenta', linestyle="dashed")
+	ax1.scatter(ex, e, c='magenta', s=12,
+    label='Evaluation CER from lstmtraining (list.eval - ' +
+    str(evallistlinecount) +' lines)', alpha=0.5)
+	annot_min('magenta',-30,30,ex,e,et)
+
+if not c.dropna().empty: # not NaN or empty
+	ax1.scatter(cx, c, c='blue', s=12,
+    label='Checkpoints CER  from lstmtraining (list.train - ' +
+    str(trainlistlinecount) +' lines)', alpha=0.5)
+	annot_min('blue',-30,-40,cx,c,ct)
+
+ax1.set_xlim([0,None])
+ax1.set_ylim([-1,maxy])
+
+def fit_spline(splinex, spliney, splinedf, splines, splinecolor):
+	if not spliney.dropna().empty: 
+		uni = UnivariateSpline(splinex, spliney, s=splines)
+		yxs = np.linspace(splinex.min(), splinex.max(), len(splinedf.index))
+		ax1.plot(yxs, uni(yxs), splinecolor, alpha=0.5)
+
+fit_spline(x, y, ydf, 500, 'teal')
+fit_spline(ex, e, edf, 5, 'magenta')
+fit_spline(vx, v, vdf, 6, 'maroon')
 
 plt.title(label=PlotTitle)
 plt.legend(loc='upper right')
